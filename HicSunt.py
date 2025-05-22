6a0c05ebd018102154fedfe4a7841e6cdf3f3da1
I. A. Guez <iacg1@st-andrews.ac.uk>
2025-05-22 14:02:30 +0100

    Update HicSunt.py
    blackbody and v.1 support update

--- HicSunt.py
+++ HicSunt.py
@@ -4,26 +4,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import re
+import sys
 from sklearn.cluster import HDBSCAN
 from sklearn.preprocessing import RobustScaler
 from scipy.spatial import ConvexHull
 from scipy import interpolate
 from scipy import fft as sft
-
+from astropy.modeling.physical_models import BlackBody 
+from astropy import units as u
 
 ##CONSTANTS
-types  = ['Error', 'Unknown', 'Archean', 'ArcheanHaze', 'E-type', 'ModernEarth', 'DryO2', 'WetO2', 'CO2', 'V-type']
-colours = ['r', 'Magenta', 'Firebrick', 'Maroon','Navy', 'b', 'SlateBlue', 'DodgerBlue', 'DarkKhaki', 'LimeGreen']
+types  = ['Error', 'Unknown', 'Archean', 'ArcheanHaze', 'E-type', 'ModernEarth', 'DryO2', 'WetO2', 'CO2', 'V-type', 'Proterozoic']
+colours = ['r', 'Magenta', 'Firebrick', 'Maroon','Navy', 'b', 'SlateBlue', 'DodgerBlue', 'DarkKhaki', 'LimeGreen', 'CadetBlue']
+
+clouds = ['Error', 'Unknown', 'clear', 'haze', 'cirrus', 'cloud', 'strato']
+cldcol = ['r', 'Indigo', 'PowderBlue', 'SlateGray', 'SkyBlue', 'SteelBlue', 'DarkBlue']
+
+spectype = ['Error', 'Unknown', 'O', 'B', 'A', 'F', 'G', 'K', 'M', 'L', 'T']
+spcol = ['r', 'Lime', 'RoyalBLue', 'LightBlue', 'Aquamarine', 'PaleGoldenrod', 'Gold', 'Orange', 'Crimson', 'DarkRed', 'Maroon']
+
 markers = ['.', '+', 'x', '*']
+umrk = ['.', '+', 'x', '*', 'v', '1']
 
-traycols = ["Temperature (K)", "Flux (S$_{e}$)", "Semi-Major Axis (mAU)", "Radius (R$_{e}$)", "Pressure (Pa)", 
-            "O$_{2}$ ", "H$_{2}$O", "CO", "CO$_{2}$", "O$_{3}$", "N$_{2}$", "CH$_{4}$", 
-            "O$_{2}$/CO$_{2}$", "H$_{2}O$/CO$_{2}$"]
-gcl = [("plasma", 0.1, 0.8), ("cmr.sepia", 0.2, 1), ("RdBu", 0.2, 0.8), ("cmr.emergency", 0.2, 0.8), ("cmr.voltage", 0.1, 0.8),
+traycols = ["Temperature (K)", "T$_{eff}$(K)", "Flux (S$_{e}$)", "Semi-Major Axis (mAU)", "Radius (R$_{e}$)", "Pressure (Pa)", 
+            "O$_{2}$", "H$_{2}$O", "CO", "CO$_{2}$", "O$_{3}$", "N$_{2}$", "CH$_{4}$", #bottom of atm
+            "O$_{2}$", "H$_{2}$O", "CO", "CO$_{2}$", "O$_{3}$", "N$_{2}$", "CH$_{4}$", #top of atm
+            "O$_{2}$/CO$_{2}$", "O$_{2}$/N$_{2}$"]
+gcl = [("plasma", 0.1, 0.8), ("cmr.sunburst", 0.4, 0.8), ("cmr.sepia", 0.2, 1), ("RdBu", 0.2, 0.8), ("cmr.emergency", 0.2, 0.8), ("cmr.voltage", 0.1, 0.8),
                 ("cmr.sapphire_r", 0.1, 0.8), ("cmr.ocean_r", 0.2, 0.8), ("cmr.fall_r", 0.1, 0.8), ("pink_r", 0.2, 1),
-                ("GnBu_r", 0.1, 0.8), ("cmr.swamp_r", 0.1, 0.8), ("cmr.neutral_r", 0.1, 0.8), ("cmr.iceburn_r", 0, 1),
-                ("cmr.voltage", 0.1, 0.8)]
+                ("GnBu_r", 0.1, 0.8), ("cmr.swamp_r", 0.1, 0.8), ("cmr.neutral_r", 0.1, 0.8), #bottom of atm
+                ("cmr.sapphire_r", 0.1, 0.8), ("cmr.ocean_r", 0.2, 0.8), ("cmr.fall_r", 0.1, 0.8), ("pink_r", 0.2, 1),
+                ("GnBu_r", 0.1, 0.8), ("cmr.swamp_r", 0.1, 0.8), ("cmr.neutral_r", 0.1, 0.8), #top of atm
+                ("cmr.iceburn_r", 0, 1),("cmr.holly", 0.1, 0.8)]
 
+teltype = ["ECLIPS-UV", "ECLIPS-VIS", "ECLIPS-NIR","NIRSpec", "MIRI"]
+telstat = [((0.2,0.5125), 7, "RFL"), ((0.515,1.03), 140, "RFL"), ((1,2),70, "TRN"), ((1,5.3), 100, "TRN"), ((5,12), 280,"TRN")] 
+#each is telstat[0]=win as a tuple, and telstat[1]=RP, telstat[2]=runtype. For use with GetTEL.
 
 ##DATA ROUTINES
 #This one is only still here for backwards compatitibility with my older code
@@ -56,9 +72,6 @@ def ExtractTRN(path, col, ext):
             pass
         total = np.resize(total, (pull.shape[0],len(sims)))  
         total[:,t] = pull[:,int(col)]
-        #print(total[0:5,:])
-        #print(total[-5:,:])
-        #print(total.shape)
         x=pull[:,0]    
         t+=1
     if x[1]<x[0]:
@@ -70,16 +83,17 @@ def ExtractTRN(path, col, ext):
         pass
     return(total, sims, x)
 
-def Extract3D(path, col, ext):
+def Extract3D(path, rt=None):
     #This pulls the planets within a folder already.
     #Col marks the collumn to pull from. 1 is Total. anything else is a specific molecule. don't do 0, that's x.
+    #for the TRN type col will be automatically set to pull the correct one from either VLPSE or PSG
     try:
         sims = sorted(next(os.walk(os.path.join(path,'.')))[1])#[0:-2]
     except StopIteration:
         pass
     total = np.zeros([len(sims), 3], dtype="object")
     #Need to start as 3D, else it gets folded like pie dough.
-    t=0
+    t=0    
     for f in sims: 
         try:
             os.chdir(path + "/" + f)
@@ -87,21 +101,35 @@ def Extract3D(path, col, ext):
             pass
         #print(os.getcwd())        
         for l in os.listdir('.'):
-            if l[-4:] == str(ext):
-                #print(l)
-                spectrum='./'+str(l)
+            if rt == "TRN":            
+                if l[-4:] == ".trn":
+                    spectrum='./'+str(l)
+                    col=2
+                elif l[-4:] == ".rad":
+                    spectrum='./'+str(l)
+                    col=4 #1 is Total, 4 is Exoplanet only, 5 is transit.
+            elif rt == "RFL":
+                if l[-4:] == ".rfl":
+                    spectrum='./'+str(l)
+                    col=3 # 2 Stellar, 3 Planetary, 4 Albedo
+                elif l[-4:] == ".irr":
+                    spectrum='./'+str(l)
+                    col=4 #1 is Total, 4 is Exoplanet only.
+            else:
+                sys.exit('Run type not specified!\nPlease add "TRN" or "RFL" rt flag to TrayTable.')
+                    
             if l[-4:] == str('.atl'):
                 stats = './'+str(l)
         #UPLOAD TABLE
         try:
             #print("Pulling...")
-            pull = np.genfromtxt(str(spectrum), dtype='float', comments='#')
+            pull = np.genfromtxt(str(spectrum), dtype='float', comments='#', filling_values=np.nan)
             hold = pull[:,int(col)]
-            physpln = np.genfromtxt(str(stats), dtype='float', comments='#', usecols=1, filling_values=np.nan)
-            #print(physpln)
+            physpln = np.genfromtxt(str(stats), dtype='float', comments='#', skip_header=2, usecols=1, filling_values=np.nan)
+            spectype = np.genfromtxt(str(stats), dtype='object', comments='#', max_rows=1, usecols=1, filling_values=np.nan)
         except:
-            print("Pull Error in {0:s}, check headers! \nTerminating Program...".format(f))
-            pass
+            #print("oops")
+            sys.exit("Pull Error in {0:s} {1:s}! \n Probable causes: Missing .atl, improper headers. \nTerminating Program...".format(f, spectrum))
         x=pull[:,0]   
         #print(x.shape)
         if x[1]<x[0]:
@@ -114,26 +142,47 @@ def Extract3D(path, col, ext):
             pass
         total[t,0] = [hold,x]
         #print(total.shape)
-        total[t,1] = Barcode(f)#[0]
+        total[t,1] = Barcode(f, spectrum, spectype)#0: atmtype, 1: cloudtype, 2:spectype
+        #print(Barcode(f, spectrum, spectype))
         total[t,2]=physpln
         t+=1
     return(total, sims)
 
-def Barcode(pan):
-    #z=0, values
-    #z=1, planet type
-    #z=2, haze indicator.
-    if str(pan) in types: 
-        code= types.index(str(pan))
+def Barcode(pan, cloud, atl):
+    #1 is used for "spare" values that aren't part of the pre-determined dataset.
+    #0 is not used to avoid issues with floats. A 0 value is an error and should be treated as such.
+    for ty in types:
+        if str(pan).find(ty)!=-1:
+            if str(pan)=="ArcheanHaze": 
+                pcode=3
+            else:
+                pcode=types.index(str(ty))
+            break
+        else:
+            pcode=1#"""
+    """if str(pan) in types: 
+        pcode=types.index(str(pan))
     else:
-        #1 is used for "spare" values that aren't part of the pre-determined dataset.
-        #0 is not used to avoid issues with floats. A 0 value is an error and should be treated as such.
-        code=1
-    return(code)
+        pcode=1#"""
+    for cld in clouds:
+        if str(cloud).find(cld)!=-1:
+            ctype=clouds.index(str(cld))
+            #print('match found: ', cld)
+            break
+        else:
+            ctype=1#"""
+    for spec in spectype:
+        if str(atl).find(spec)!=-1:
+            stype=spectype.index(str(spec))
+            break
+        else:
+            stype=1
+    return([pcode, ctype, stype])
 
-def TrayTable(path, bands, subres=None, win=(5,12)):
+def TrayTable(path, rt, sd, bands, subres=None, win=(5,12), SNR=None):
     # 0 = Temperature, 1 = Flux, 2 = Semi-major Axis, 3 = Radius, 4+: individual gas concentrations (rough)
     #This is a temporary kluge to get around an error.
+    #hr is "human readable", will output a .masc txt file as well as a .npy file
     try:
         print("Extracting VPLSE Data...")
         sims = sorted(next(os.walk(os.path.join(path,'.')))[1])
@@ -147,28 +196,57 @@ def TrayTable(path, bands, subres=None, win=(5,12)):
         os.chdir(path + "/" + f)  
         mile=os.getcwd()
         simin=sims.index(f)
-        tot, pans= Extract3D(mile, 2, ".trn")
+        tot, pans= Extract3D(mile, rt) #extract spectrum 0 (2 item list), barcode 1 (2 item list), Mixing ratios 2 (array)
         #print("shape ", np.shape(tot))
         for i in range(0,len(pans)):
-            A0=Normalise(tot[i,0][0])
-            x0=tot[i,0][1]
-            A, x, h = Unresolve(A0, x0, win, subres)
-            try:
-                hold[:,1,i]=tot[i,1]
-            except IndexError:
-                hold=np.pad(hold,((0,0),(0,0),(0,1)),'constant')          
-                hold[:,1,i]=tot[i,1]
+            print("     Calculating: ", pans[i])
+            if rt == "RFL":
+                bby = BBAdjust(tot[i,0][0], tot[i,0][1], tot[i,2][1])[0] #perform the blackbody adjustment
+            else:
+                bby = tot[i,0][0] #skip BBAdjust for TRN which is already a dimensionless ratio
+            A0=Normalise(bby) #normalise y values of spectrum.
+            try: 
+                A, x, h = Unresolve(A0, tot[i,0][1], win, subres) #math both
+                A, x = AddNoise(A, x, win, SNR, sd)
+            except: 
+                print("Skipping ", f, i)
+                pass
             for tup in bands:
-                y, xt = Window(x, A, tup)
-                ar = np.trapz(y, xt)
-                #print('ar', ar)
-                hold[bands.index(tup),0,i]=ar
-                hold[bands.index(tup),2,i]=tot[i,2]
-            #print('hold', hold[:,:,i])
-        #print(hold.shape)
+                try:
+                    #print(tot[i,1])
+                    hold[bands.index(tup),1,i]=tot[i,1] #put barcode in storage in allx-1sty-ithz
+                except IndexError:
+                    hold=np.pad(hold,((0,0),(0,0),(0,1)),'constant') #add a line       
+                    hold[bands.index(tup),1,i]=tot[i,1] #keep going
+                    
+                y, xt = Window(x, A, tup) #isolate band from spectrum
+                ar = np.trapz(y, xt) #calculate area
+                hold[bands.index(tup),0,i]=ar #store area in bandx-0thy-ithz
+                hold[bands.index(tup),2,i]=tot[i,2] #store MRs on the 2ndy to keep them and move on.
         tray[simin]=hold
-    print("VPLSE Data Loaded")
+    print("VPLSE Data Loaded")  # The tray is a list of the hold[x=band*s*, y=area|barcode|MRs, z=atmosphere*s*] per sim
     return(tray, sims)
+
+def LoadTray(rt, sd, bands, subres, win, SNR):
+    try:
+        tray = np.load('C:/Users/Lyan/StAtmos/HSD/Test/Trays/BBProv/{0}-{1}x{2}_{3}({4}){5}S{6}E.npy'.format(win[0], win[1], bands, rt, sd, subres, SNR), 
+               allow_pickle=True)
+        sims = np.genfromtxt('C:/Users/Lyan/StAtmos/HSD/Test/Trays/VPLSE.sims', dtype='str', delimiter=',', comments='#') 
+    except FileNotFoundError:
+        sys.exit(".npy file not found. Please run TrayTable first.")
+    print('Tray Loaded')
+    return(tray, sims)
+
+def GetTEL(name, nbands=10, SNR=5):
+    telstats=telstat[teltype.index(name)]
+    #this is going to return [*win,RP,RT]
+    rt=telstats[2]
+    sd=14
+    subres=telstats[1]
+    win=telstats[0]    
+    bands=Bandwidth(telstats[0], nbands)
+    tray, sims = LoadTray(rt, sd, nbands, subres, win, SNR) #Load the correct Tray.
+    return(bands, tray, sims, SNR)    
 
 def RLDataSingle(path, bands, ints=1):
     print("Extracting Observatory Data...")
@@ -196,7 +274,6 @@ def RLDataAuto(infile, bands, ints=1):
         f.seek(0)
         ln=0
         for line in f:
-            #print(line)
             lclean=str(line.rstrip("\n"))
             hdul = fits.open(lclean, memmap=True, ignore_missing_simple=True)
             for n in range(0,ints):
@@ -213,33 +290,45 @@ def RLDataAuto(infile, bands, ints=1):
             hdul.close()
     print("Observatory Data Loaded")
     return(hold)
+
+def SelectCell(dmv, numcells=100):
+    #Default is 100 because there are never going to be that many valid cells under the curren paradigm
+    #therefore all of them are printed.
+    hold= np.loadtxt(dmv, dtype='float', comments='#', delimiter=' ', skiprows=0, usecols=(0,1,3,4)) #pull the values from dmv
+    names = np.loadtxt(dmv, dtype='str', comments='#', delimiter=' ', skiprows=0, usecols=(2)) #pull the names from dmv
+    DBmin=np.absolute(hold[:,2]).argsort()
+    NRmin=hold[:,3].argsort()
+    for x in range(0, hold.shape[0]):
+        if hold[x,3]>1:
+            hold[x,3]=hold[x,3]/10
+        else:
+            pass
+    hold= np.pad(hold,((0,0),(0,1)),'constant') #add a collumn to store the score       
+    hold[:,4]=np.absolute(hold[:,2]-hold[:,3])
+    score=hold[:,4].argsort()
+    return(names[score][:numcells], hold[score][:numcells])
     
 ## PROCESSING ROUTINES
 def Normalise(y):
-    mn=min(y)
-    mx=max(y)
-    #print(mn, mx)
+    ny=y#np.ma.masked_invalid(y)
+    mn=min(ny)
+    mx=max(ny)
     for l in range(0,len(y)):    
         y[l] = (y[l]-mn)/(mx-mn)
     return(y)
 
 def Recut(x,step=300):
-    #print("OG: ", len(x))
     gap=len(x)/step
-    #print(gap)
     newx=[]
     for i in range(0, step):
         newx.append(x[round(gap*i)])
-    #print("New: ",len(newx))
     return(newx) 
  
 
 def Smooth(wave, a=0, b=-1, rev=False):
     out = sft.fft(wave)
-    #print("Full", len(out))
     if rev==True:
         out = sft.ifft(out[a:b])
-        #print("Cut", len(out))
         return(out)
     else:
         return(out)
@@ -254,7 +343,7 @@ def Window(x, y, bounds=(0,1)):
             #print(x[i])
             window.append(y[i]) 
             sill.append(x[i])
-    #print(window, sill)          
+    #print("win", window[:5], "\nsill", sill[:5])          
     return(window, sill)
 
 def Bandwidth(spec, nbands):
@@ -266,11 +355,11 @@ def Bandwidth(spec, nbands):
         bands.append((synthspec[b], synthspec[b+1]))
     return(bands)    
 
-def Unresolve(iny, inx, win=(5,12), subres=None):
-        #win is the window, defaults to 5-12 as we've been doing before with MIRI
+def Unresolve(iny, inx, win, subres=None):
+        if win == None: sys.exit("Please specify a window.")
+        
         A=Normalise(iny)
-        x=inx
-        ya, xa = Window(x, A, (win[0], win[1]))
+        ya, xa = Window(inx, A, (win[0], win[1]))
         if subres!=None and len(xa)-1>subres:
             F = Normalise(Smooth(ya, 0, subres, rev=True).real)
             newx=Recut(xa, subres)
@@ -288,18 +377,42 @@ def Unresolve(iny, inx, win=(5,12), subres=None):
             health=0
         return(F, newx, health)
 
-def MRS_R(wvl):
-    wvl=np.array(wvl)
-    R=4603-128*wvl-10**(-7.4*wvl)
-    return(R)
+def AddNoise(y, x, win, SNR=None, sd=14):
+    if SNR==None:
+        ya, xa = y, x
+        noise=np.zeros((len(ya)))
+    else:
+        A=Normalise(y)
+        ya, xa = Window(x, A, (win[0], win[1]))        
+        mn=np.mean(ya)
+        #stv=np.nanstd(ya)
+        mn_noise=mn/SNR
+        #print("Mean: ", mn, ", Noise: ", mn_noise)
+        np.random.seed(sd)
+        noise = np.random.normal(0, mn_noise, len(ya))
+    return(Normalise(ya+noise), xa)
 
+def BBAdjust(y, x, Teff): #Blackbody ajustment for VIS bands.
+    TGU = u.W / (u.m ** 2 * u.um * u.sr)
+    bb= BlackBody(temperature= Teff*u.K, scale=1.0*TGU)
+    flux=bb(x*u.um)
+    Px=bb.lambda_max.to(u.um, equivalencies=u.spectral()).value
+    step=np.absolute(x[90]-x[89])
+    S = y[np.where(np.isclose(x, Px, rtol=20*step))][0]
+    P = max(flux.value)
+    C=S/P
+
+    fxadj=C*flux
+    div = Normalise(y/fxadj.value)
+    return(div, flux)
 
 ## PLOTTING ROUTINES
 def PareDown(ax, tup):
     handles, labels = ax.get_legend_handles_labels()
     unique = [(h, l) for i, (h, l) in enumerate(zip(handles, labels)) if l not in labels[:i]]
     ax.legend(*zip(*unique), bbox_to_anchor=tup, loc='upper left', borderaxespad=0)
-def ContourCluster(xy):
+    
+def ContourCluster(xy): #not currrently used due to oddities with each run
     #Run HDBSCAN
     vals= RobustScaler().fit_transform(xy)
     mpts=0.02
@@ -369,6 +482,5 @@ def DrawContour(xy, col, plot=True):
             return(interp_x, interp_y)
 
 def StripAlnum(str):
-    #used to create nice labels using the lists in the Constants section
     fl=re.sub(r'[^a-zA-Z0-9]', "", str)
     return(fl)
