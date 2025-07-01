812b732e9b59dd72744bbbea855622024e48236f
I. A. Guez <iacg1@st-andrews.ac.uk>
2025-07-01 13:26:23 +0100

    Update RDBSMulti.py

--- RDBSMulti.py
+++ RDBSMulti.py
@@ -6,7 +6,8 @@ import sys
 from sklearn.cluster import HDBSCAN
 from sklearn.preprocessing import RobustScaler
 import DBCV as valid
-from collections import Counter
+import BatchID as display
+import string
 
 """"
 RDBS (A, B, ukn, overlist, code, bst)
@@ -50,39 +51,16 @@ def GetOL(tray, source, code):
         if code==0:
             label=hic.types[ol]
             c=hic.colours[ol]
-            marker=hic.markers[ol%(len(hic.markers))]
-        elif code==1:
-            label=hic.clouds[ol]
-            c=hic.cldcol[ol]
-            marker='D'
-        elif code==2:
-            label=hic.spectype[ol]
-            c=hic.spcol[ol]
-            marker='*'
-        else:
-            sys.exit("Code {0} not valid. \n Correct values are: Atmospheres (0), Clouds (1), Spectral Types (2).".format(code))
-    if source=="prior":
-        #print('Overlay type is "prior".')
-        if code>20: sys.exit("Code {0} out of range. Valid codes are:".format(code) +
-                             "\n0: Temperature, 1: T_eff, 2: Flux, 3: SemJ, 4: R, 5: P" +
-                             "\n6: O2, 7: H2O, 8: CO, 9: CO2, 10: O3, 11: N2, 12: CH4 | Bottom of Atmosphere" +
-                             "\n13: O2.1, 14: H2O.1, 15: CO.1, 16: CO2.1, 17: O3.1, 18: N2.1, 19: CH4.1 |Top of Atmosphere"
-                             "\n20: O2/CO2")
-        elif code==20: #Will only do O2/CO2
-            ol=(tray[6]/tray[9])
-        else:
-            ol=tray[code]
-        label=hic.traycols[code]
-        c=ol
-        cmap=None
-        marker='o'
-    
-    return(np.array([ol, label, c, marker, cmap]))
+##CONSTANTS
+ukn=-5 #No. of unknowns in the dataset, a negative number please
+#They *need* to be at the end of the alphabetised Sims list.
+#Just put a Z in front of their name or something.
 
-def RDBS (A, B, ukn, overlist, code, bst):
+def RDBS (A, B, ukn, overlist, code, bst, top=0, batch=False):
     #First, go fetch the trays and store their data.
-    bandsA, TrayA, simsA, SNRA  = GetTEL(A)
-    bandsB, TrayB, simsB, SNRB = GetTEL(B)
+    imprt=np.load('C:/Users/Lyan/StAtmos/HSD/Test/Trays/Imprint.npy', allow_pickle=True) #[s][p]
+    bandsA, TrayA, simsA, SNRA  = hic.GetTRAY(A)
+    bandsB, TrayB = hic.GetTRAY(B)[:2]
     noA=len(bandsA) #x, vertical
     noB=len(bandsB) #y, horizontal 
         
@@ -92,15 +70,16 @@ def RDBS (A, B, ukn, overlist, code, bst):
     ##If these fail, something is wrong and the program won't try to run. 
     #So fix these issues first, then the rest can run and waste your time in a different way
     
+    
     ##Input
     #Takes either a single integer for "Best Cell" or a tuple for a specific cell    
     if isinstance(bst, int)==True:
         #cell[$1][row index of SelectCell, usually 0:best][$0 or 1]
         print("Method: Auto SelectCell")
-        dmv='C:/Users/Lyan/StAtmos/HSD/Test/Trays/Multi/{0}{1}v{2}{3}_S{4}-{5}{6}.dmv'.format(A,noA,B,noB,SNRA,overlist,code)
+        dmv='C:/Users/Lyan/StAtmos/HSD/Test/Trays/Multi/dmvmulti/{0}{1}v{2}{3}_S{4}-{5}{6}.dmv'.format(A,noA,B,noB,SNRA,overlist,code)
         cell=hic.SelectCell(dmv)
-        v=int(cell[1][bst][0]) 
-        h=int(cell[1][bst][1])
+        v=int(cell[bst,0]) 
+        h=int(cell[bst,1])
     else:
         print("Method: Manual Cell ID")
         v=bst[0]
@@ -109,12 +88,13 @@ def RDBS (A, B, ukn, overlist, code, bst):
     print('Calculations in Progress. Please wait...')
     fig, ax = plt.subplot_mosaic("A;B") #remember to put A;B back in afterwards.
     plt.subplots_adjust(left=0.1, right=0.82, top=0.88, bottom=0.11)
-    fig.set_size_inches(9,7);
+    fig.set_size_inches(8,6);
 
     #Sample the trays
     vA=[]
     hB=[]
     sims=[]
+    ip=[]
     overlay=np.zeros((1,4), dtype='object')
     arch=np.zeros((1,4), dtype='object')
 
@@ -124,18 +104,21 @@ def RDBS (A, B, ukn, overlist, code, bst):
             vA.append(TrayA[s][v,0,p]) #[star][band, "area", atm] 
             hB.append(TrayB[s][h,0,p])
             sims.append(s)
+            ip.append(imprt[s][p])
             
-            arch=np.row_stack((arch,GetOL(TrayA[s][0,1,p], "barcode", 0)[:-1])) 
-            if overlist in ["spec", "clouds", "bar"] and code!=0:
-                overlay=np.row_stack((overlay,GetOL(TrayA[s][0,1,p], "barcode", code)[:-1]))                    
+            arch=np.row_stack((arch,hic.GetOL(TrayA[s][0,1,p], "barcode", top)[:-1])) 
+            if overlist in ["arch", "spec", "clouds", "bar"] and code!=top:
+                overlay=np.row_stack((overlay,hic.GetOL(TrayA[s][0,1,p], "barcode", code)[:-1]))                    
             elif overlist=="atlas":
-                overlay=np.row_stack((overlay,GetOL(TrayA[s][0,2,p], "prior", code)[:-1]))
+                overlay=np.row_stack((overlay,hic.GetOL(TrayA[s][0,2,p], "prior", code)[:-1]))
             else:
                 sys.exit("{0} is an invalid overlay.".format(overlist)+
-                         "\nValid lists are: arch, spec, clouds and atlas")
+                         "\nValid lists are: arch, spec, clouds, bar and atlas"+
+                         "\nCode must not equal top={0}".format(top))
     #Store samples and prevent algorithmic indigestion
     vals= np.column_stack((vA, hB))
     vals= RobustScaler().fit_transform(vals)
+    ip=np.array(ip)
     overlay=np.delete(overlay,0,0)
     arch=np.delete(arch,0,0)
     
@@ -146,6 +129,7 @@ def RDBS (A, B, ukn, overlist, code, bst):
     for w in range(len(overlay)+(ukn+1), len(overlay)): #Unknowns
         ax["A"].scatter(vals[w,0],vals[w, 1], s=80,
                         c="Magenta", marker=hic.umrk[w%(len(hic.umrk))], zorder=5)
+        #ax["A"].annotate(str(simsA[sims[w]]), xy=(vals[w,0], vals[w,1]), xytext=(1.5, 1.5), textcoords='offset points', fontsize=8)
     if ukn!=0: ax["A"].scatter(vals[ukn,0],vals[ukn, 1], c="Cyan", s=80, zorder=10, marker=hic.umrk[1%(len(hic.umrk))])
 
     
@@ -153,16 +137,18 @@ def RDBS (A, B, ukn, overlist, code, bst):
         if code>=6: barscl='log' 
         else: barscl='linear'
         cmap = cmr.get_sub_cmap(*hic.gcl[code])
-        temp=ax["B"].scatter(vals[:ukn,0],vals[:ukn, 1], s=15,
+        temp=ax["B"].scatter(vals[:ukn,0],vals[:ukn, 1], s=80,
                         c=overlay[:ukn,2], cmap=cmap, marker=overlay[0,3], zorder=5, norm=barscl)
-        ax["B"].scatter(vals[ukn+1:,0],vals[ukn+1:, 1], s=15, 
+        ax["B"].scatter(vals[ukn+1:,0],vals[ukn+1:, 1], s=80, 
                         c= "Magenta", marker= '+', zorder=10)
-        ax["B"].scatter(vals[ukn,0],vals[ukn, 1], s=15,
+        ax["B"].scatter(vals[ukn,0],vals[ukn, 1], s=80,
                         c="Cyan", marker='+', zorder=10)
     else:
-        temp=ax["B"].scatter(vals[:,0],vals[:, 1], s=15,
-                        c=overlay[:,2], cmap=cmap, marker=overlay[:,3], zorder=5)
-
+        if code>=6: barscl='log' 
+        else: barscl='linear'
+        cmap = cmr.get_sub_cmap(*hic.gcl[code])
+        temp=ax["B"].scatter(vals[:,0],vals[:, 1], s=80,
+                        c=overlay[:,2], cmap=cmap, marker=overlay[0,3], zorder=5, norm=barscl)
 
     mpts=0.02
     mcls=0.15
@@ -173,10 +159,10 @@ def RDBS (A, B, ukn, overlist, code, bst):
     unique_labels = set(labels)
     centroids=db.centroids_
 
-                    
     score =[]
     color = [plt.cm.tab20b(each) for each in np.linspace(0, 1, len(unique_labels))]
-    #Creates a colourmap for the size of our dataset
+    tree = np.zeros([len(unique_labels)], dtype="object")
+    abc=list(string.ascii_uppercase)[:len(unique_labels)]
     for k, col in zip(unique_labels, color):
         class_member_mask = labels == k
         if k == -1:
@@ -192,21 +178,31 @@ def RDBS (A, B, ukn, overlist, code, bst):
             except:
                 score="0%"            
             xy = vals[class_member_mask]
+            tree[k]=ip[class_member_mask]
             ctr=hic.DrawContour(xy, col, False)
             ax["B"].fill(ctr[0], ctr[1], '--', c=col, alpha=0.4, zorder=1) 
             ax["B"].annotate(score, xy=(centroids[k,0],centroids[k, 1]), 
                              xytext=(-k*3,+25), textcoords='offset points', fontsize=20, zorder=7)
-    
+            ax["B"].annotate(abc[k], xy=(centroids[k,0],centroids[k, 1]), c=col,
+                             fontsize=20, zorder=7)
     ## PLOT LAYOUT
     label = hic.traycols[code]
     if  code < 6:
         fl = hic.StripAlnum(label)[:3]
+        #for j in range(0,vals.shape[0]):
+            #ax["B"].annotate('{0:.2f}'.format(overlay[j,0]), xy=(vals[j,0], vals[j,1]), xytext=(2, 2), textcoords='offset points', fontsize=6)#"""
+
     elif 6 <= code < 20:
         fl = hic.StripAlnum(label)        
         label=label+" Mixing Ratio"
+        for j in range(0,vals.shape[0]):
+            ax["B"].annotate('{0:.2E}'.format(overlay[j,0]), xy=(vals[j,0], vals[j,1]), xytext=(2, 2), textcoords='offset points', fontsize=6)#"""
     else:
         fl = hic.StripAlnum(label)
         label="Normalised "+label+" Ratio"
+        for j in range(0,vals.shape[0]):
+            ax["B"].annotate('{0:.2E}'.format(overlay[j,0]), xy=(vals[j,0], vals[j,1]), xytext=(2, 2), textcoords='offset points', fontsize=6)#"""
+
 
     print("Calculating {0} Validity...".format(fl))
     validity=valid.DBCV(vals, labels)
@@ -226,9 +222,18 @@ def RDBS (A, B, ukn, overlist, code, bst):
                      textcoords='offset fontsize', fontsize='medium', verticalalignment='top')
     ax["B"].annotate('b)',xy=(0, 1), xycoords='axes fraction', xytext=(+0.5, -0.5), 
                      textcoords='offset fontsize', fontsize='medium', verticalalignment='top')    
-    plt.colorbar(temp, ax=[ax["A"],ax["B"]], anchor=(1.51,0), shrink=0.45, label=str(label))
-    plt.show()
-    return(filename)
-  
+    if overlist=="atlas":
+        plt.colorbar(temp, ax=[ax["A"],ax["B"]], anchor=(1.51,0), shrink=0.45, label=str(label))
+        
+    if batch==True: 
+        if A!=B:
+            display.BatchID(tree, A, v, 10, 5)
+            display.BatchID(tree, B, h, 10, 5)
+        else: display.BatchID(tree, A, bst, 10, 5)
+    else:
+        plt.show()
+        
+    return(filename, (v,h))
 
-RDBS("MIRI", "MIRI", 5, "atlas", 20, bst=(2,6))
+
+RDBS("MIRI", "NIRSpec", 5, "atlas", 6, bst=(4,0), top=2, batch=True)
